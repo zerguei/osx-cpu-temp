0a3a8bfb93425ca2f1e63f86ce0212968d204916
Serguei Kisliak <serguei.kisliak@gmail.com>
2021-07-09 20:03:21 +0200

    if fan name is empty - set it to name-0

--- smc.c
+++ smc.c
@@ -251,18 +251,17 @@ void readAndPrintFanRPMs(void)
 
         printf("Num fans: %d\n", totalFans);
         for (i = 0; i < totalFans; i++) {
+
             sprintf(key, "F%dID", i);
             result = SMCReadKey(key, &val);
             if (result != kIOReturnSuccess) {
                 continue;
             }
             char name[32];
-            //char* name = val.bytes + 4;
             strncpy(name, val.bytes+4, 32);
-            if (strlen(name) < 1) {
+            if (strlen(name) < 1) { // if retrieved name is "" - set if to "name-0", "name-1", ...
                 snprintf(name, 32, "name-%d", i);
             }
-            //printf(">> name: %s;  len %d\n", name, strlen(name));
 
             sprintf(key, "F%dAc", i);
             float actual_speed = SMCGetFanRPM(key);
